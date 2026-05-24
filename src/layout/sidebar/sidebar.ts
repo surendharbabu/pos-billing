8c4d588a9e7e6c8623934ed9991b26358c870b0d
surendhar <surendharbabu2296@gmail.com>
2026-05-24 14:28:22 +0530

    sidebar changes

--- src/layout/sidebar/sidebar.ts
+++ src/layout/sidebar/sidebar.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink],
+  imports: [RouterLink,RouterModule,CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
-export class Sidebar {}
+export class Sidebar {
+
+  openMenu: string = '';
+}
